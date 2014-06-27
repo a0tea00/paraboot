5954317cb836442d8cc50fdb3a0db0f845a1b6ae
Trey Huang <thuang2@albany.edu>
2014-06-27 13:35:31 -0400

    combined css using sass

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-//= require bootstrap/button
+function toScrollFixedTop(selector, offset){
+var mainNav = $(selector);
+var areaY = $(window).height()*offset; 
+
+		var isFixed = true;
+		$(window).resize(function(){
+			areaY = $(window).height();
+		});
+
+		$(window).scroll( function() {	
+		var doFix = $(window).scrollTop() > areaY;	
+
+		if (doFix && !isFixed) {
+		mainNav.hide().addClass('yellow-navtop-fixedtop').fadeIn();
+		isFixed = true;
+		}
+		if (!doFix && isFixed){
+		mainNav.hide().removeClass('yellow-navtop-fixedtop').fadeIn();
+		isFixed = false;
+		}
+
+		});
+}
